912ba8ae459ab24a6c3ca38d2b3112045faff9ae
victorbasilva <102388646+victorbasilva@users.noreply.github.com>
2023-12-15 14:01:45 +0000

    Development V2.0

--- src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
+++ src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
@@ -2,14 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package aadp_lab_soccersimulator;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
+
 
 /**
  *
@@ -25,204 +19,72 @@ import java.util.Scanner;
  * Name 3: Alessandra Cabelliero
  * Number 3: 2021258
  * 
- * Name 4:
- * Number 4
- * 
  */
+
+package aadp_lab_soccersimulator;
+
+import java.sql.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class AADP_Lab_SoccerSimulator {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    private static final String DB_URL = "jdbc:mysql://localhost/";
+    private static final String USER = "football";
+    private static final String PASS = "Java is almost as good as football";
+
+    public static void main(String[] args) {
         String dbName = "world_cup";
         String[] teams = {"Ireland", "Brazil", "Argentina", "Japan", "Mexico", "Senegal", "Tunisia", "Qatar"};
-        String DB_URL = "jdbc:mysql://localhost/" + dbName;
-        String USER = "football";
-        String PASS = "Java is almost as good as football";
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PASS);
-            Statement stmt = conn.createStatement();
-            stmt.execute("CREATE SCHEMA IF NOT EXISTS " + dbName +";");
-            stmt.execute("USE " + dbName + ";");
-            for (String team : teams) {
-                stmt.execute(
-                        "CREATE TABLE IF NOT EXISTS "+ team + " ("
-                                + "name VARCHAR(30) NOT NULL,"
-                                + "number INT NOT NULL PRIMARY KEY,"
-                                + "birth VARCHAR(30),"
-                                + "position VARCHAR(30),"
-                                + "goalsScored INT,"
-                                + "background TEXT(1000));"
-                );                
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        int option;
+
+        DatabaseManager.createSchemaAndTables(dbName, teams);
+
         boolean exit = false;
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         do {
             System.out.println("Welcome! What would you like to do today? Please enter the number corresponding to your choice.");
             System.out.println("1. Enter a new player to a team.");
             System.out.println("2. See the players on a team.");
             System.out.println("3. Simulate a number of matches.");
-            System.out.println("4. Exit the programme.");            
-            try {
-                option = Integer.parseInt(sc.nextLine());
-                if (option == 1) {
-                    boolean validTeam = false;
-                    String teamName;
-                    System.out.println("Please follow the instructions to enter player data.");
-                    do {
-                        System.out.println("For which team would you like to enter data?");
-                        teamName = sc.nextLine();
-                        for (String team : teams) {
-                            if (teamName.toLowerCase().equals(team.toLowerCase())) {
-                                validTeam = true;
-                                break;
-                            }
-                        }
-                        if (teamName.toLowerCase().equals("exit")) break;
-                        if (!validTeam) System.out.println("That is not one of the teams. Please try again!");
-                    } while (!validTeam);
-                    String name;
-                    int number = 0;
-                    String birth;
-                    String position;
-                    int goalsScored = 0;
-                    String background;
-                    boolean validPlayer = false;
-                    System.out.println("Please enter the player's name: ");
-                    name = sc.nextLine();
-                    System.out.println("Please enter the player's number: ");
-                    do {
-                        try {
-                            number = Integer.parseInt(sc.nextLine()); 
-                            if (number < 1) {
-                                System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+            System.out.println("4. Exit the program.");
 
-                        } catch (Exception e) {
-                            System.out.println("That is not a number. please try again!");
-                        }
-                    } while (!validPlayer);                                              
-                    System.out.println("Please enter the player's date of birth: ");
-                    birth = sc.nextLine();         
-                    System.out.println("Please enter the player's position: ");
-                    position = sc.nextLine(); 
-                    System.out.println("Please enter the number of goals the player has scored: ");
-                    validPlayer = false;
-                    do {
-                        try {
-                            goalsScored = Integer.parseInt(sc.nextLine()); 
-                            if (goalsScored < 1) {
-                                System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+            int option = InputValidator.validatePositiveIntegerInput(scanner);
 
-                        } catch (Exception e) {
-                            System.out.println("That is not a number. please try again!");
-                        }
-                    } while (!validPlayer);     
-                    System.out.println("Please enter the player's background: ");
-                    background = sc.nextLine();                          
-                    System.out.println("Thank you for entering a player"); 
-                    try {
-                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                        Statement stmt = conn.createStatement();
-                        stmt.execute(
-                                String.format("INSERT INTO %s (name, number, birth, position, goalsScored, background) "
-                                        + "VALUES (\"%s\", %d, \"%s\", \"%s\", %d,  \"%s\") ;",
-                                        teamName, name, number, birth, position, goalsScored, background)
-                        );
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }      
-                } else if (option == 2) {
-                    boolean validTeam = false;
-                    String teamName;
-                    System.out.println("Please follow the instructions to get player data.");
-                    do {
-                        System.out.println("For which team would you like to get player data?");
-                        teamName = sc.nextLine();
-                        for (String team : teams) {
-                            if (teamName.toLowerCase().equals(team.toLowerCase())) {
-                                validTeam = true;
-                                break;
-                            }
-                        }
-                        if (teamName.toLowerCase().equals("exit")) break;
-                        if (!validTeam) System.out.println("That is not one of the teams. Please try again!");
-                    } while (!validTeam);
-                    try {
-                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                        Statement stmt = conn.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT * from " + teamName + ";");
-                        String name;
-                        int number;
-                        String birth;
-                        String position;
-                        int goalsScored;
-                        String background;
-                        while (rs.next()) {
-                            name = rs.getString("name");
-                            number = rs.getInt("number");
-                            birth = rs.getString("birth");
-                            position = rs.getString("position");
-                            goalsScored = rs.getInt("goalsScored");
-                            background = rs.getString("background");              
-                            System.out.println(String.format("Name: %s -- Number: %d -- DoB: %s -- Position: %s -- Number of goals scored: %d", name, number, birth, position, goalsScored));
-                            System.out.println("Background:");
-                            System.out.println(background);
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }                     
-                } else if (option == 3) {
-                    System.out.println("How many matches would you like to simulate?");
-                    Boolean validNum = false;
-                    int numMatches = 0;
-                    do {
-                        try {
-                            numMatches = Integer.parseInt(sc.nextLine()); 
-                            if (numMatches < 1) {
-                                System.out.println("Please enter a positive integer");
-                            } else validNum = true;
-
-                        } catch (Exception e) {
-                            System.out.println("That is not a number. please try again!");
-                        }
-                    } while (!validNum);
-                    for (int matchNum = 1; matchNum <= numMatches; matchNum++) {
-                        int team1Num = (int) (Math.floor(Math.random() * teams.length));
-                        String team1 = teams[team1Num];
-                        int team2Num;
-                        do {
-                            team2Num = (int) (Math.floor(Math.random() * teams.length));
-                        } while (team1Num == team2Num);
-                        String team2 = teams[team2Num];
-                        int team1Score = (int) (Math.floor(Math.random() * 6));
-                        int team2Score = (int) (Math.floor(Math.random() * 6));
-                        System.out.println("Time for mathch: " + matchNum);
-                        if (team1Score > team2Score) {
-                            System.out.println(String.format("Congratulation %s! %s scored %d goals and %s scored %d goals.", team1, team1, team1Score, team2, team2Score));
-                        } else if (team1Score < team2Score) {
-                            System.out.println(String.format("Congratulation %s! %s scored %d goals and %s scored %d goals.", team2, team1, team1Score, team2, team2Score));
-                        } else {
-                            System.out.println(String.format("It was a draw!! %s scored %d goals and %s scored %d goals.",  team1, team1Score, team2, team2Score));
-                        }
-                    }
-                } else if (option == 4) {
+            switch (option) {
+                case 1:
+                    enterNewPlayer(teams, scanner);
+                    break;
+                case 2:
+                    viewPlayersOnTeam(teams, scanner);
+                    break;
+                case 3:
+                    simulateMatches(teams, scanner);
+                    break;
+                case 4:
                     System.out.println("Goodbye, and thank you for using the SoccerSimulator!");
                     exit = true;
-                } else {
+                    break;
+                default:
                     System.out.println("That is an invalid number. Please try again!");
-                }
-            } catch (Exception e) {
-                System.out.println("That is not a number. Please try again!");
-            }         
-        } while (!exit);   
-    }     
+                    break;
+            }
+
+        } while (!exit);
+    }
+
+    private static void enterNewPlayer(String[] teams, Scanner scanner) {
+        // Logic for entering a new player
+        // Use DatabaseManager.insertPlayer() to insert data into the database
+    }
+
+    private static void viewPlayersOnTeam(String[] teams, Scanner scanner) {
+        // Logic for viewing players on a team
+        // Use DatabaseManager to retrieve and display player data
+    }
+
+    private static void simulateMatches(String[] teams, Scanner scanner) {
+        // Logic for simulating matches
+        // Use MatchSimulator.simulateMatches() to simulate matches
+    }
 }
--- src/aadp_lab_soccersimulator/DatabaseManager.java
+++ src/aadp_lab_soccersimulator/DatabaseManager.java
@@ -14,64 +14,74 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseManager {
-    private static final String DB_URL = "jdbc:mysql://localhost/";
-    private static final String USER = "football";
-    private static final String PASS = "Java is almost as good as football";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USER, PASS);
+    public static void createSchemaAndTables(String dbName, String[] teams) {
+        Connection connection = null;
+        try {
+            connection = connect(dbName);
+
+            createSchema(connection, dbName);
+            useDatabase(connection, dbName);
+
+            for (String team : teams) {
+                createTable(connection, team);
+            }
+        } finally {
+            closeConnection(connection);
+        }
     }
 
-    public static void createSchema(String dbName) {
-        try (Connection conn = getConnection(); Statement stmt = conn.createStatement()) {
-            stmt.execute("CREATE SCHEMA IF NOT EXISTS " + dbName + ";");
+    private static Connection connect(String dbName) {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/";
+            connection = DriverManager.getConnection(url, "football", "Java is almost as good as football");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void createSchema(Connection connection, String dbName) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE SCHEMA IF NOT EXISTS " + dbName + ";");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void useDatabase(String dbName) {
-        try (Connection conn = getConnection(); Statement stmt = conn.createStatement()) {
-            stmt.execute("USE " + dbName + ";");
+    private static void useDatabase(Connection connection, String dbName) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("USE " + dbName + ";");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void createTable(String teamName) {
-        String createTableQuery = String.format(
-                "CREATE TABLE IF NOT EXISTS %s ("
-                        + "name VARCHAR(30) NOT NULL,"
-                        + "number INT NOT NULL PRIMARY KEY,"
-                        + "birth VARCHAR(30),"
-                        + "position VARCHAR(30),"
-                        + "goalsScored INT,"
-                        + "background TEXT(1000));", teamName);
-
-        try (Connection conn = getConnection(); Statement stmt = conn.createStatement()) {
-            stmt.execute(createTableQuery);
+    private static void createTable(Connection connection, String team) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS " + team + " ("
+                            + "name VARCHAR(30) NOT NULL,"
+                            + "number INT NOT NULL PRIMARY KEY,"
+                            + "birth VARCHAR(30),"
+                            + "position VARCHAR(30),"
+                            + "goalsScored INT,"
+                            + "background TEXT(1000));"
+            );
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void insertPlayer(String teamName, Player player) {
-        String insertPlayerQuery = String.format(
-                "INSERT INTO %s (name, number, birth, position, goalsScored, background) "
-                        + "VALUES (\"%s\", %d, \"%s\", \"%s\", %d,  \"%s\") ;",
-                teamName, player.getName(), player.getNumber(), player.getBirth(), player.getPosition(),
-                player.getGoalsScored(), player.getBackground());
-
-        try (Connection conn = getConnection(); Statement stmt = conn.createStatement()) {
-            stmt.execute(insertPlayerQuery);
+    private static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
-    // Add more methods as needed
 }
-
-
-
-
